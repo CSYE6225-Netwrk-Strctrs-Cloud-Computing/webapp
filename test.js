21eb90e71b69bee72bd3da0fc4ca59101963949f
Tanuj Kodali <tanujkodali@Tanujs-MacBook-Pro-2.local>
2024-10-15 19:39:40 -0400

    cheching

--- test.js
+++ test.js
@@ -20,22 +20,6 @@ describe('API Tests', () => {
                 expect(res.status).to.equal(200);
             });
     });
-    describe('User Retrieval', () => {
-        it('should retrieve user information successfully', async () => {
-            
-            const res = await request(app)
-                .get('/v1/user/self')
-                .set('Authorization', `Basic ${Buffer.from('test@example.com:TestPassword1').toString('base64')}`);
-            expect(res.status).to.equal(200);
-            expect(res.body).to.have.property('emailAddress', 'test@example.com');
-        });
-
-        it('should return 401 for unauthorized access', async () => {
-            const res = await request(app).get('/v1/user/self');
-            expect(res.status).to.equal(401);
-        });
-    });
-
    
 });
 });
